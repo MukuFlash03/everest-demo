3f8e874c52b7a8425002db38eba069c73617336a
Mahadik, Mukul Chandrakant <MukulChandrakant.Mahadik@nrel.gov>
2024-07-03 09:31:32 -0700

    Consolidating changes to match PR branch

--- manager/run-test.sh
+++ manager/run-test.sh
@@ -1,5 +1,3 @@
 #! /bin/sh
-cat ./misc_test.txt
-echo "Not running tests"
-# pip install pytest
-# pytest --everest-prefix ../build/dist core_tests/startup_tests.py 
+pip install pytest
+pytest --everest-prefix ../build/dist core_tests/startup_tests.py 
