46b8cd54cbe8f9197930cd4b5af5fee5251b8ef1
Mahadik, Mukul Chandrakant <MukulChandrakant.Mahadik@nrel.gov>
2024-07-03 07:31:42 -0700

    Added build flag to up command + Added echo statements to demo script

--- demo-automated-testing.sh
+++ demo-automated-testing.sh
@@ -32,17 +32,21 @@ download_demo_file .env
 
 docker images
 
+echo "Running docker compose build..."
 docker compose --project-name everest-ac-demo \
 	       --file "${DEMO_DIR}/${DEMO_COMPOSE_FILE_NAME}" build \
 
+echo "Running docker compose up..."
 docker compose --project-name everest-ac-demo \
 	       --file "${DEMO_DIR}/${DEMO_COMPOSE_FILE_NAME}" up \
+           --build \
            --abort-on-container-exit \
            --exit-code-from manager
 
 exit_code=$?            
 echo "Docker-compose up exit code from manager service: $exit_code"       
 
+echo "Running docker compose down..."
 docker compose --project-name everest-ac-demo \
 	       --file "${DEMO_DIR}/${DEMO_COMPOSE_FILE_NAME}" down
 
