aa8b772eb014ccd30fd030f676af9661c32c19e9
Mahadik, Mukul Chandrakant <MukulChandrakant.Mahadik@nrel.gov>
2024-07-02 23:39:01 -0700

    Resetting exit code and Setting push value of first build job to false
    First build job should never push images even in case of non-PR events.
    Testing should occur after first build.
    Then push should occur in 2nd build job.

--- demo-automated-testing.sh
+++ demo-automated-testing.sh
@@ -45,5 +45,4 @@ echo "Docker-compose up exit code from manager service: $exit_code"
 docker-compose --project-name everest-ac-demo \
 	       --file "${DEMO_DIR}/${DEMO_COMPOSE_FILE_NAME}" down
 
-# exit $exit_code
-exit 1
+exit $exit_code
