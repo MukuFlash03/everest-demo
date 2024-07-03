b25cf4eabdcca98f911e094fd39efddd14d6e5ce
Mahadik, Mukul Chandrakant <MukulChandrakant.Mahadik@nrel.gov>
2024-07-02 23:59:07 -0700

    Testing if changes to run-test.sh reflected

--- manager/run-test.sh
+++ manager/run-test.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 cat ./misc_test.txt
-pip install pytest
-pytest --everest-prefix ../build/dist core_tests/startup_tests.py 
+echo "Not running tests"
+# pip install pytest
+# pytest --everest-prefix ../build/dist core_tests/startup_tests.py 
