c3539aaeb17b84b2609464019e9a1ba9e0e0dd61
Mukul Chandrakant Mahadik <mmahadik@asu.edu>
2024-07-15 16:02:31 -0700

    Including test inside docker build push job
    Docker build push action documentation had an example to include test step.
    
    Build first without pushing.
    Test.
    Push.
    
    One concern is that since we have matrix jobs, the latest built images might not be available for a matrix job that is still building an image for a service while another service in another matrix job needs the first service's image and ends up using the current available image before the one being built in the pipeline.
    
    Signed-off-by: Mahadik, Mukul Chandrakant <MukulChandrakant.Mahadik@nrel.gov>

--- manager/run-test.sh
+++ manager/run-test.sh
@@ -1,5 +1,3 @@
 #! /bin/sh
-# pip install pytest
-# pytest --everest-prefix ../build/dist core_tests/startup_tests.py 
-
-cat test-exit-code.sh
+pip install pytest
+pytest --everest-prefix ../build/dist core_tests/startup_tests.py 
--- manager/test-cat.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-echo "Printing contents of test-exit-code.sh"
-cat test-exit-code.sh
